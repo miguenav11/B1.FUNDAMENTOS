21a46e592fc373a94d6bed40b54cc626c49cf450
Miguel Ángel Navarro <miguelanav01@gmail.com>
2025-10-18 13:04:20 +0200

    Clase 3

--- DOCKER/main_docker.py
+++ DOCKER/main_docker.py
@@ -1,1 +1,19 @@
-print("Soy un crack en Docker, un animal vamos")
+import requests
+import numpy as np
+# from time import sleep
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+suma = arr1 + arr2
+print(suma)
+print(TODO)
+
+# def primera_letra(word):
+#     return word[0]
+# print(primera_letra("Docker"))
+
+# response = requests.get("https://catfact.ninja/fact")
+# data = response.json()
+# print(data)
+# print(data["fact"])
+# print(data.get("fact", "default"))
